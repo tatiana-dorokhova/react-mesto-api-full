10a63457dab7234465c9d084307c8e507f96ab83
Tatiana Dorokhova <tatjana.dorokhova@yandex.ru>
2023-02-16 21:09:15 +0300

    15PR новая сборка фронта

--- frontend/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer () {
-    return (
-      <footer className="footer section">
-        <p className="footer__text">© 2022 Mesto Russia</p>
-     </footer>
-    );
-}
-
-export default Footer;
